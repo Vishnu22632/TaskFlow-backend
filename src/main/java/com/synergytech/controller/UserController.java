f507da8d767f3da4718361d98d8268362d9295fa
Vishnu Thakur <vishnu22632@gmail.com>
2024-11-09 04:44:34 +0545

    api for crud operation completed

--- src/main/java/com/synergytech/controller/UserController.java
+++ src/main/java/com/synergytech/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.synergytech.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +40,39 @@ public class UserController {
 		
 		return new ResponseEntity<>(users,HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id){
+		Optional<User> user = userService.getUserById(id);
+		
+		if(user.isPresent()) {
+			return new ResponseEntity<>(user.get(),HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+		Optional<User> user = userService.getUserById(id);
+		
+		if(user.isPresent()) {
+			userService.deleteUser(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/synergytech/service/UserService.java
+++ src/main/java/com/synergytech/service/UserService.java
@@ -1,6 +1,7 @@
 package com.synergytech.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,21 @@ public class UserService {
 		List<User> users = userRepository.findAll();
 		return users;
 	}
-
+	
+	public Optional<User> getUserById(Long id){
+		 return userRepository.findById(id);
+	}
+	
+	public void deleteUser(Long id) {
+		userRepository.deleteById(id);
+	}
 }
+
+
+
+
+
+
+
+
+
